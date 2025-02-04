5a7b9a7acd8188ad388b3f4b02c3890057b560a1
Nathan-ma <than.matos@gmail.com>
2025-02-04 11:26:45 -0300

    Run formatter

--- TimeAlarms.h
+++ TimeAlarms.h
@@ -4,11 +4,12 @@
 #define TimeAlarms_h
 
 #include <Arduino.h>
+
 #include "TimeLib.h"
 
-#if !defined(dtNBR_ALARMS )
+#if !defined(dtNBR_ALARMS)
 #if defined(__AVR__)
-#define dtNBR_ALARMS 6   // max is 255
+#define dtNBR_ALARMS 6  // max is 255
 #elif defined(ESP8266)
 #define dtNBR_ALARMS 20  // for esp8266 chip - max is 255
 #else
@@ -18,21 +19,16 @@
 
 #define USE_SPECIALIST_METHODS  // define this for testing
 
-typedef enum {
-  dtMillisecond,
-  dtSecond,
-  dtMinute,
-  dtHour,
-  dtDay
-} dtUnits_t;
+typedef enum { dtMillisecond, dtSecond, dtMinute, dtHour, dtDay } dtUnits_t;
 
 typedef struct {
-  uint8_t alarmType      :4 ;  // enumeration of daily/weekly (in future:
-                               // biweekly/semimonthly/monthly/annual)
-                               // note that the current API only supports daily
-                               // or weekly alarm periods
-  uint8_t isEnabled      :1 ;  // the timer is only actioned if isEnabled is true
-  uint8_t isOneShot      :1 ;  // the timer will be de-allocated after trigger is processed
+  uint8_t alarmType : 4;  // enumeration of daily/weekly (in future:
+                          // biweekly/semimonthly/monthly/annual)
+                          // note that the current API only supports daily
+                          // or weekly alarm periods
+  uint8_t isEnabled : 1;  // the timer is only actioned if isEnabled is true
+  uint8_t isOneShot : 1;  // the timer will be de-allocated after trigger is
+                          // processed
 } AlarmMode_t;
 
 // new time based alarms should be added just before dtLastAlarmType
@@ -43,25 +39,25 @@ typedef enum {
   dtDailyAlarm,
   dtWeeklyAlarm,
   dtLastAlarmType
-} dtAlarmPeriod_t ; // in future: dtBiweekly, dtMonthly, dtAnnual
+} dtAlarmPeriod_t;  // in future: dtBiweekly, dtMonthly, dtAnnual
 
-// macro to return true if the given type is a time based alarm, false if timer or not allocated
-#define dtIsAlarm(_type_)  (_type_ >= dtExplicitAlarm && _type_ < dtLastAlarmType)
-#define dtUseAbsoluteValue(_type_)  (_type_ == dtTimer || _type_ == dtExplicitAlarm)
+// macro to return true if the given type is a time based alarm, false if timer
+// or not allocated
+#define dtIsAlarm(_type_) (_type_ >= dtExplicitAlarm && _type_ < dtLastAlarmType)
+#define dtUseAbsoluteValue(_type_) (_type_ == dtTimer || _type_ == dtExplicitAlarm)
 
 typedef uint8_t AlarmID_t;
 typedef AlarmID_t AlarmId;  // Arduino friendly name
 
 #define dtINVALID_ALARM_ID 255
-#define dtINVALID_TIME     (time_t)(-1)
+#define dtINVALID_TIME (time_t)(-1)
 #define AlarmHMS(_hr_, _min_, _sec_) (_hr_ * SECS_PER_HOUR + _min_ * SECS_PER_MIN + _sec_)
 
 typedef void (*OnTick_t)();  // alarm callback function typedef
 
 // class defining an alarm instance, only used by dtAlarmsClass
-class AlarmClass
-{
-public:
+class AlarmClass {
+ public:
   AlarmClass();
   OnTick_t onTickHandler;
   void updateNextTrigger();
@@ -71,16 +67,16 @@ public:
 };
 
 // class containing the collection of alarms
-class TimeAlarmsClass
-{
-private:
+class TimeAlarmsClass {
+ private:
   AlarmClass Alarm[dtNBR_ALARMS];
   void serviceAlarms();
   uint8_t isServicing;
-  uint8_t servicedAlarmId; // the alarm currently being serviced
-  AlarmID_t create(time_t value, OnTick_t onTickHandler, uint8_t isOneShot, dtAlarmPeriod_t alarmType);
+  uint8_t servicedAlarmId;  // the alarm currently being serviced
+  AlarmID_t create(
+      time_t value, OnTick_t onTickHandler, uint8_t isOneShot, dtAlarmPeriod_t alarmType);
 
-public:
+ public:
   TimeAlarmsClass();
   // functions to create alarms and timers
 
@@ -96,12 +92,13 @@ public:
     return create(value, onTickHandler, true, dtDailyAlarm);
   }
   AlarmID_t alarmOnce(const int H, const int M, const int S, OnTick_t onTickHandler) {
-    return alarmOnce(AlarmHMS(H,M,S), onTickHandler);
+    return alarmOnce(AlarmHMS(H, M, S), onTickHandler);
   }
 
   // trigger once on a given day and time
-  AlarmID_t alarmOnce(const timeDayOfWeek_t DOW, const int H, const int M, const int S, OnTick_t onTickHandler) {
-    time_t value = (DOW-1) * SECS_PER_DAY + AlarmHMS(H,M,S);
+  AlarmID_t alarmOnce(
+      const timeDayOfWeek_t DOW, const int H, const int M, const int S, OnTick_t onTickHandler) {
+    time_t value = (DOW - 1) * SECS_PER_DAY + AlarmHMS(H, M, S);
     if (value <= 0) return dtINVALID_ALARM_ID;
     return create(value, onTickHandler, true, dtWeeklyAlarm);
   }
@@ -112,12 +109,13 @@ public:
     return create(value, onTickHandler, false, dtDailyAlarm);
   }
   AlarmID_t alarmRepeat(const int H, const int M, const int S, OnTick_t onTickHandler) {
-    return alarmRepeat(AlarmHMS(H,M,S), onTickHandler);
+    return alarmRepeat(AlarmHMS(H, M, S), onTickHandler);
   }
 
   // trigger weekly at a specific day and time
-  AlarmID_t alarmRepeat(const timeDayOfWeek_t DOW, const int H, const int M, const int S, OnTick_t onTickHandler) {
-    time_t value = (DOW-1) * SECS_PER_DAY + AlarmHMS(H,M,S);
+  AlarmID_t alarmRepeat(
+      const timeDayOfWeek_t DOW, const int H, const int M, const int S, OnTick_t onTickHandler) {
+    time_t value = (DOW - 1) * SECS_PER_DAY + AlarmHMS(H, M, S);
     if (value <= 0) return dtINVALID_ALARM_ID;
     return create(value, onTickHandler, false, dtWeeklyAlarm);
   }
@@ -128,7 +126,7 @@ public:
     return create(value, onTickHandler, true, dtTimer);
   }
   AlarmID_t timerOnce(const int H, const int M, const int S, OnTick_t onTickHandler) {
-    return timerOnce(AlarmHMS(H,M,S), onTickHandler);
+    return timerOnce(AlarmHMS(H, M, S), onTickHandler);
   }
 
   // trigger at a regular interval
@@ -136,36 +134,46 @@ public:
     if (value <= 0) return dtINVALID_ALARM_ID;
     return create(value, onTickHandler, false, dtTimer);
   }
-  AlarmID_t timerRepeat(const int H,  const int M,  const int S, OnTick_t onTickHandler) {
-    return timerRepeat(AlarmHMS(H,M,S), onTickHandler);
+  AlarmID_t timerRepeat(const int H, const int M, const int S, OnTick_t onTickHandler) {
+    return timerRepeat(AlarmHMS(H, M, S), onTickHandler);
   }
 
   void delay(unsigned long ms);
 
   // utility methods
-  uint8_t getDigitsNow( dtUnits_t Units) const;         // returns the current digit value for the given time unit
-  void waitForDigits( uint8_t Digits, dtUnits_t Units);
+  uint8_t getDigitsNow(
+      dtUnits_t Units) const;  // returns the current digit value for the given time unit
+  void waitForDigits(uint8_t Digits, dtUnits_t Units);
   void waitForRollover(dtUnits_t Units);
 
   // low level methods
-  void enable(AlarmID_t ID);                // enable the alarm to trigger
-  void disable(AlarmID_t ID);               // prevent the alarm from triggering
-  AlarmID_t getTriggeredAlarmId() const;          // returns the currently triggered  alarm id
-  bool getIsServicing() const;                    // returns isServicing
-  void write(AlarmID_t ID, time_t value);   // write the value (and enable) the alarm with the given ID
-  time_t read(AlarmID_t ID) const;                // return the value for the given timer
-  dtAlarmPeriod_t readType(AlarmID_t ID) const;   // return the alarm type for the given alarm ID
+  void enable(AlarmID_t ID);              // enable the alarm to trigger
+  void disable(AlarmID_t ID);             // prevent the alarm from triggering
+  AlarmID_t getTriggeredAlarmId() const;  // returns the currently triggered  alarm id
+  bool getIsServicing() const;            // returns isServicing
+  void write(
+      AlarmID_t ID,
+      time_t value);                // write the value (and enable) the alarm with the given ID
+  time_t read(AlarmID_t ID) const;  // return the value for the given timer
+  dtAlarmPeriod_t readType(AlarmID_t ID) const;  // return the alarm type for the given alarm ID
 
-  void free(AlarmID_t ID);                  // free the id to allow its reuse
+  void free(AlarmID_t ID);  // free the id to allow its reuse
 
 #ifndef USE_SPECIALIST_METHODS
-private:  // the following methods are for testing and are not documented as part of the standard library
+ private:  // the following methods are for testing and are not documented as part
+           // of the standard library
 #endif
-  uint8_t count() const;                          // returns the number of allocated timers
-  time_t getNextTrigger() const;                  // returns the time of the next scheduled alarm
-  time_t getNextTrigger(AlarmID_t ID) const;      // returns the time of scheduled alarm
-  bool isAllocated(AlarmID_t ID) const;           // returns true if this id is allocated
-  bool isAlarm(AlarmID_t ID) const;               // returns true if id is for a time based alarm, false if its a timer or not allocated
+  uint8_t count() const;  // returns the number of allocated timers
+  void writeNextTrigger(AlarmID_t ID, time_t time) {
+    if (isAllocated(ID)) {
+      Alarm[ID].nextTrigger = time;
+    }
+  }
+  time_t getNextTrigger() const;              // returns the time of the next scheduled alarm
+  time_t getNextTrigger(AlarmID_t ID) const;  // returns the time of scheduled alarm
+  bool isAllocated(AlarmID_t ID) const;       // returns true if this id is allocated
+  bool isAlarm(AlarmID_t ID) const;           // returns true if id is for a time based alarm,
+                                              // false if its a timer or not allocated
 };
 
 extern TimeAlarmsClass Alarm;  // make an instance for the user
@@ -175,13 +183,12 @@ extern TimeAlarmsClass Alarm;  // make an instance for the user
  *============================================================================*/
 
 /* public */
-#define waitUntilThisSecond(_val_) waitForDigits( _val_, dtSecond)
-#define waitUntilThisMinute(_val_) waitForDigits( _val_, dtMinute)
-#define waitUntilThisHour(_val_)   waitForDigits( _val_, dtHour)
-#define waitUntilThisDay(_val_)    waitForDigits( _val_, dtDay)
+#define waitUntilThisSecond(_val_) waitForDigits(_val_, dtSecond)
+#define waitUntilThisMinute(_val_) waitForDigits(_val_, dtMinute)
+#define waitUntilThisHour(_val_) waitForDigits(_val_, dtHour)
+#define waitUntilThisDay(_val_) waitForDigits(_val_, dtDay)
 #define waitMinuteRollover() waitForRollover(dtSecond)
-#define waitHourRollover()   waitForRollover(dtMinute)
-#define waitDayRollover()    waitForRollover(dtHour)
-
+#define waitHourRollover() waitForRollover(dtMinute)
+#define waitDayRollover() waitForRollover(dtHour)
 
 #endif /* TimeAlarms_h */
